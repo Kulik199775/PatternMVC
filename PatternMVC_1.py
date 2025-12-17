1826a32696131a3b0122d5f694507ca66ec27b3c
Dmitriy Kulikov <dimonkulikov@mail.ru>
2025-12-17 22:27:31 +0300

    Добавлен класс магический метод для представления, добавлен класс модель обуви, добавлен метод добавления новой обуви

--- PatternMVC_1.py
+++ PatternMVC_1.py
@@ -20,3 +20,18 @@ class Shoe:
             'manufacturer': self.manufacturer,
             'size': self.size
         }
+
+    def __str__(self):
+        return f'{self.shoe_type} {self.shoe_kind} ({self.color}),размер {self.size}, цена {self.price} руб.'
+
+class ShoeModel:
+    def __init__(self):
+        self.shoes = []
+        self.next_id = 1
+
+    def add_shoe(self, shoe_type, shoe_kind, color, price, manufacturer, size):
+        """Добавить новую обувь"""
+        shoe = Shoe(self.next_id, shoe_type, shoe_kind, color, price, manufacturer, size)
+        self.shoes.append(shoe)
+        self.next_id += 1
+        return shoe
