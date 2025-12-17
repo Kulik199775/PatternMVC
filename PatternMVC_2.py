4490b532910cf54150ea75e9734794e7d7a23cf0
Dmitriy Kulikov <dimonkulikov@mail.ru>
2025-12-18 00:17:59 +0300

    Добавлен класс рецептов

--- PatternMVC_2.py
+++ PatternMVC_2.py
@@ -9,3 +9,19 @@ class Recipe:
         self.cuisine = cuisine
         self.video_link = video_link
 
+    def to_dict(self):
+        """Преобразование объекта в словарь"""
+        return {
+            'id': self.recipe_id,
+            'name': self.name,
+            'author': self.author,
+            'type': self.recipe_type,
+            'description': self.description,
+            'ingredients': self.ingredients,
+            'cuisine': self.cuisine,
+            'video_link': self.video_link
+        }
+
+    def __str__(self):
+        ingredients_str = ', '.join(self.ingredients[:3]) + ("..." if len(self.ingredients) > 3 else "")
+        return f"{self.name} ({self.recipe_type}) - {self.cuisine} кухня"
