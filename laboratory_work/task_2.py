018566c977849dba68386dd87ac228ac12e95ee6
Dmitriy Kulikov <dimonkulikov@mail.ru>
2025-12-21 23:05:29 +0300

    Добавлен класс статья для представления модели данных

---
+++ laboratory_work/task_2.py
@@ -0,0 +1,47 @@
+# модель
+class Article:
+    """Класс Статья, представляющий модель данных"""
+
+    def __init__(self, title: str, author: str, char_count: int,
+                 publication: str, description: str = ""):
+
+        self.title = title
+        self.author = author
+        self.char_count = char_count
+        self.publication = publication
+        self.description = description
+
+    def update(self, title: str = None, author: str = None,
+               char_count: int = None, publication: str = None,
+               description: str = None):
+        """Обновляет информацию о статье"""
+
+        if title:
+            self.title = title
+        if author:
+            self.author = author
+        if char_count:
+            self.char_count = char_count
+        if publication:
+            self.publication = publication
+        if description:
+            self.description = description
+
+    def to_dict(self) -> dict:
+        """Преобразует статью в словарь"""
+
+        return {
+            'title': self.title,
+            'author': self.author,
+            'char_count': self.char_count,
+            'publication': self.publication,
+            'description': self.description
+        }
+
+    def __str__(self) -> str:
+        """Строковое представление статьи."""
+        return (f"Статья: {self.title}\n"
+                f"Автор: {self.author}\n"
+                f"Количество знаков: {self.char_count}\n"
+                f"Издание: {self.publication}\n"
+                f"Описание: {self.description}")
